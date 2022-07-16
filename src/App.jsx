6b7f45d0e07c82c73e73036cf72f33576c3191a0
Moudhaffer <moudhaffer.bouallegui@esprit.tn>
2022-07-16 14:53:31 +0100

    Tempalte & Flask API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.scss';
 import FileUpload from './FileUpload/FileUpload';
 import FileList from './FileList/FileList';
 
 function App() {
   const [files, setFiles] = useState([])
+  const [initialData, setInitialData] = useState([{}])
 
   const removeFile = (filename) => {
     setFiles(files.filter(file => file.name !== filename))
   }
+
+  useEffect(() => {
+    fetch('/details').then(
+      response => response.json()
+    ).then(data => setInitialData(data))
+  }, []);
 
   return (
     <div className="App">
@@ -16,6 +23,11 @@ function App() {
       <FileUpload files={files} setFiles={setFiles}
         removeFile={removeFile} />
       <FileList files={files} removeFile={removeFile} />
+      <div>
+        <h3>Hello {initialData.name}</h3>
+        <h4>Email: {initialData.email}</h4>
+        <h4>Email: {initialData.degree}</h4>
+      </div>
     </div>
   );
 }
