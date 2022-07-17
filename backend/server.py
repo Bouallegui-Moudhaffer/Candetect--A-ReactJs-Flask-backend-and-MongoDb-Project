36d5507f44446d278af14608d0d9d6d38445d38b
Moudhaffer <moudhaffer.bouallegui@esprit.tn>
2022-07-17 17:37:29 +0100

    Fixing backend Frontend Integration

--- backend/server.py
+++ backend/server.py
@@ -1,25 +1,55 @@
+from flask import g
 from flask import Flask
+from flask import request
 from pyresparser import ResumeParser
+import pyrebase
+import os.path
+
+config = {
+    "apiKey": "AIzaSyAVMKtmiL9Mxj9tK9lj4HppzKO09iLC6pE",
+    "authDomain": "candetect-838ef.firebaseapp.com",
+    "databaseURL": "https://candetect-838ef-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "candetect-838ef",
+    "storageBucket": "candetect-838ef.appspot.com",
+    "messagingSenderId": "136984825672",
+    "appId": "1:136984825672:web:034077fe53e3b58aa46d7d",
+    "measurementId": "G-YWGBWPM5B2"
+  }
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
 
 
 app = Flask(__name__)
 
-# Details API Route
+
+# File download Link
+@app.route('/filePath', methods=['POST'])
+def get_path():
+    data = request.get_json()["path"]
+    storage.child(f"files/{data}").download(f"files/Resume.pdf")
+    return "Success.."
+    
+
+
+# Details API Get Route
 @app.route("/details")
 def details():
-    resume_data = ResumeParser(r'D:\React\Candetect\backend\Uploaded_Resumes\OmkarResume.pdf').get_extracted_data()
-    return {"college_name":resume_data.get('college_name'),
-            "company_names":resume_data.get('company_names'),
-            "degree":resume_data.get('degree'),
-            "designation":resume_data.get('designation'),
-            "email":resume_data.get('email'),
-            "experience":resume_data.get('experience'),
-            "mobile_number":resume_data.get('mobile_number'),
-            "name":resume_data.get('name'),
-            "no_of_pages":resume_data.get('no_of_pages'),
-            "skills":resume_data.get('skills'),
-            "total_experience":resume_data.get('total_experience')
-    }
+    path = r"D:\React\Candetect\backend\files\Resume.pdf"
+    if os.path.exists(path):
+        resume_data = ResumeParser(path).get_extracted_data()
+        return {"college_name":resume_data.get('college_name'),
+                "company_names":resume_data.get('company_names'),
+                "degree":resume_data.get('degree'),
+                "designation":resume_data.get('designation'),
+                "email":resume_data.get('email'),
+                "experience":resume_data.get('experience'),
+                "mobile_number":resume_data.get('mobile_number'),
+                "name":resume_data.get('name'),
+                "no_of_pages":resume_data.get('no_of_pages'),
+                "skills":resume_data.get('skills'),
+                "total_experience":resume_data.get('total_experience')
+        }
 
 if __name__== "__main__":
     app.run(debug=True)
--- src/FileUpload/FileUpload.jsx
+++ src/FileUpload/FileUpload.jsx
@@ -1,41 +1,64 @@
-import React from 'react'
+import { useState, useEffect, React } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import './FileUpload.scss'
-import axios from 'axios'
+import axios from 'axios';
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { storage } from "./firebase";
 
-const FileUpload = ({ files, setFiles, removeFile }) => {
-    const uploadHandler = (event) => {
-        const file = event.target.files[0];
-        if(!file) return;
-        file.isUploading = true;
-        setFiles([...files, file])
 
-        // upload file
-        const formData = new FormData();
-        formData.append(
-            "newFile",
-            file,
-            file.name
-        )
-        axios.post('http://localhost:8080/upload', formData)
-            .then((res) => {
-                file.isUploading = false;
-                setFiles([...files, file])
-            })
-            .catch((err) => {
-                // inform the user
-                console.error(err)
-                removeFile(file.name)
-            });
+const FileUpload = ({}) => {
+
+    const [progress, setProgress] = useState(0);
+    const formHandler = (e) => {
+      e.preventDefault();
+      const file = e.target.files[0];
+      uploadFiles(file);
+      ExtractHandler();
+    };
+    const [initialData, setInitialData] = useState([{}])
+
+    const ExtractHandler = () => {
+      useEffect(() => {
+        fetch('/details').then(
+          response => response.json()
+        ).then(data => setInitialData(data))
+      }, []);
     }
+
+    const uploadFiles = (file) => {
+      //
+      if (!file) return;
+      const storageRef = ref(storage, `files/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+  
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const prog = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(prog);
+        },
+        (error) => console.log(error),
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            axios.post('http://localhost:3000/filePath', {
+              'path': file.name
+            }).then(() => console.log(file.name));
+          });
+        }
+      );
+    };
+
+    
 
     return (
         <>
             <div className="file-card">
 
                 <div className="file-inputs">
-                    <input type="file" onChange={uploadHandler} />
+                    <input type="file" onChange={formHandler} />
                     <button>
                         <i>
                             <FontAwesomeIcon icon={faPlus} />
@@ -43,10 +66,13 @@ const FileUpload = ({ files, setFiles, removeFile }) => {
                         Upload
                     </button>
                 </div>
-
+                <h3>Progress {progress}</h3>
                 <p className="main">Supported files</p>
                 <p className="info">PDF, JPG, PNG</p>
 
+            </div>
+            <div>
+              <h3>Hello {initialData.name} </h3>
             </div>
         </>
     )
